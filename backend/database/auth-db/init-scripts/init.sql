a8d9b2236acd5ecffbc8fc661dd334335b7bbe94
Diego Dmitry <diegodmitry@hotmail.com>
2024-09-14 16:55:26 +0100

    changed auth-db init file

--- backend/database/auth-db/init-scripts/init.sql
+++ backend/database/auth-db/init-scripts/init.sql
@@ -3,3 +3,8 @@ CREATE TABLE users (
     email VARCHAR(100) UNIQUE NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+-- Insert sample data into users table
+INSERT INTO users (email) VALUES 
+('diegodmitry@hotmail.com'),
+('aiassistant2510@gmail.com');
